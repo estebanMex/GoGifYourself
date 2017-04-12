6672d52ac1558d489e19abf9f9cee568efdb9a62
Sam Rakoto <samyr.rakoto@gmail.com>
2017-04-13 01:24:21 +0200

    [FRONT-END] Put bootstrap, jquery and put "Freelancer" bootstrap theme by StartBootsrap (MIT Licence), added basic layout and nav menu.

--- src/GGY/AppBundle/Controller/HomeController.php
+++ src/GGY/AppBundle/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace GGY\AppBundle\Controller;
 
+use GGY\AppBundle\GGYAppBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $gifs = $em->getRepository('GGYDataBundle:Gif')->findAll();
-        return $this->render('GGYAppBundle:Home:index.html.twig', array('gifs' => $gifs));
+        $gifs = $em->getRepository("GGYDataBundle:Gif")->findAll();
+        return $this->render(
+            'GGYAppBundle:Home:index.html.twig',
+            array('gifs' => $gifs)
+        );
     }
 
 }
