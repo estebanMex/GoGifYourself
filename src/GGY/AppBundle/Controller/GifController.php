e4d66637843c55f8a803b78e8af776419fce9832
Sam Rakoto <samyr.rakoto@gmail.com>
2017-05-17 00:54:02 +0200

    [APP] Displaying tags and gif by tag slug

--- src/GGY/AppBundle/Controller/GifController.php
+++ src/GGY/AppBundle/Controller/GifController.php
@@ -24,12 +24,26 @@ class GifController extends Controller
      * @Route("/category/{slug}", name="gif_by_category_slug")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getByCategorySlug($slug){
+    public function getByCategorySlugAction($slug){
         $em = $this->getDoctrine()->getManager();
         $category = $em->getRepository("GGYDataBundle:Category")->findOneBySlug($slug);
         $gifs = $category->getGifs();
         return $this->render('GGYAppBundle:Gif:index.html.twig', array(
             'title' => $category->getTitle(),
+            'gifs' => $gifs
+        ));
+    }
+
+    /**
+     * @Route("/tag/{slug}", name="gif_by_tag_slug")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getByTagSlugAction($slug){
+        $em = $this->getDoctrine()->getManager();
+        $tag = $em->getRepository("GGYDataBundle:Tag")->findOneBySlug($slug);
+        $gifs = $tag->getGifs();
+        return $this->render('GGYAppBundle:Gif:index.html.twig', array(
+            'title' => $tag->getTitle(),
             'gifs' => $gifs
         ));
     }
--- src/GGY/AppBundle/Controller/TagController.php
+++ src/GGY/AppBundle/Controller/TagController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class TagController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="list_tags")
      */
     public function listAction()
     {
